2c0bd0df810b2b91c61cfed65034fb1fab9b2596
Himalee <himalee.tailor@gmail.com>
2018-05-04 17:46:07 +0100

    Checks verse includes a lyric

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,2 +1,12 @@
 class TenGreenBottles
-end 
+
+  def self.verse(number) # class method
+    number = 10
+
+    "10 green bottles hanging on the wall
+10 green bottles hanging on the wall
+And if one green bottle should accidentally fall,
+There'll be 9 green bottles hanging on the wall."
+  end
+
+end
--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -2,4 +2,18 @@ require "bottles"
 
 describe TenGreenBottles do
 
-end 
+  context "Display verse" do
+    
+    it "displays first verse with 10 bottles" do
+      expect(TenGreenBottles.verse(10)).to eql("10 green bottles hanging on the wall
+10 green bottles hanging on the wall
+And if one green bottle should accidentally fall,
+There'll be 9 green bottles hanging on the wall.")
+    end
+  end
+
+  it "displays all verses" do
+    expect(TenGreenBottles.verse(10)).to include("10 green bottles", "9 green")
+  end
+
+end
