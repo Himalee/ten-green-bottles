6a6e4607cac063c75bec76f8b53678c600e7ab0b
Himalee <himalee.tailor@gmail.com>
2018-05-08 12:37:00 +0100

    Refactored code

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,12 +1,11 @@
 class TenGreenBottles
 
-  def self.verse(number) # class method
-    number = 10
-
-    "10 green bottles hanging on the wall
-10 green bottles hanging on the wall
-And if one green bottle should accidentally fall,
-There'll be 9 green bottles hanging on the wall."
+  def self.first_lines
+    "10 green bottles hanging on the wall" 
   end
+
+
+
+
 
 end
--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -3,17 +3,15 @@ require "bottles"
 describe TenGreenBottles do
 
   context "Display verse" do
-    
-    it "displays first verse with 10 bottles" do
-      expect(TenGreenBottles.verse(10)).to eql("10 green bottles hanging on the wall
-10 green bottles hanging on the wall
-And if one green bottle should accidentally fall,
-There'll be 9 green bottles hanging on the wall.")
+
+    it "displays first line of verse" do
+      expect(TenGreenBottles.first_lines).to eql("10 green bottles hanging on the wall")
+    end
+
+    it "displays first and second line of verse" do
+      expect(TenGreenBottles.first_lines).to eql("10 green bottles hanging on the wall")
     end
   end
 
-  it "displays all verses" do
-    expect(TenGreenBottles.verse(10)).to include("10 green bottles", "9 green")
-  end
 
 end
