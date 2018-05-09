cf0640abc8b5ab12fc5b8a4186ae622f0cbd930f
Himalee <himalee.tailor@gmail.com>
2018-05-09 09:22:19 +0100

    Separated out verses

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,10 +1,37 @@
 class TenGreenBottles
 
-  def self.first_lines
-    "10 green bottles hanging on the wall" 
+  def initialize
+    @number = 10
   end
 
+  def number_of_bottles
+    if @number >= 3
+    regular_verse
+  else
+    singular_verse
+  end
+  end
 
+  def regular_verse
+    "#{@number} green bottles hanging on the wall
+#{@number} green bottles hanging on the wall
+And if 1 green bottle should accidentally fall,
+There'll be #{@number - 1} green bottles hanging on the wall."
+  end
+
+  def singular_verse
+    "2 green bottles hanging on the wall
+2 green bottles hanging on the wall
+And if 1 green bottle should accidentally fall,
+There'll be 1 green bottle hanging on the wall."
+  end
+
+  def final_verse
+    "1 green bottle hanging on the wall
+1 green bottle hanging on the wall
+And if 1 green bottle should accidentally fall,
+There'll be no more bottles hanging on the wall."
+  end
 
 
 
--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -4,13 +4,38 @@ describe TenGreenBottles do
 
   context "Display verse" do
 
-    it "displays first line of verse" do
-      expect(TenGreenBottles.first_lines).to eql("10 green bottles hanging on the wall")
+    it "displays regular verse" do
+      bottles = TenGreenBottles.new
+      expect(bottles.regular_verse).to eql("10 green bottles hanging on the wall
+10 green bottles hanging on the wall
+And if 1 green bottle should accidentally fall,
+There'll be 9 green bottles hanging on the wall.")
     end
 
-    it "displays first and second line of verse" do
-      expect(TenGreenBottles.first_lines).to eql("10 green bottles hanging on the wall")
+    it "displays verse when 2 bottles left" do
+      bottles = TenGreenBottles.new
+      expect(bottles.singular_verse).to eql("2 green bottles hanging on the wall
+2 green bottles hanging on the wall
+And if 1 green bottle should accidentally fall,
+There'll be 1 green bottle hanging on the wall.")
     end
+
+    it "displays verse when 1 bottle is left" do
+      bottles = TenGreenBottles.new
+      expect(bottles.final_verse).to eql("1 green bottle hanging on the wall
+1 green bottle hanging on the wall
+And if 1 green bottle should accidentally fall,
+There'll be no more bottles hanging on the wall.")
+    end
+
+    it "displays verse with 10 bottles" do
+      bottles = TenGreenBottles.new
+      expect(bottles.number_of_bottles).to eql("10 green bottles hanging on the wall
+10 green bottles hanging on the wall
+And if 1 green bottle should accidentally fall,
+There'll be 9 green bottles hanging on the wall.")
+    end
+
   end
 
 
