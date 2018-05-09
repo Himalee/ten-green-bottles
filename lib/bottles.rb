e432f7ab4d6edf1ad5adf59856980ea32942978c
Himalee <himalee.tailor@gmail.com>
2018-05-09 11:07:18 +0100

    Use of initialize variable when creating a new instance of the class TenGreenBottles

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,15 +1,18 @@
 class TenGreenBottles
 
-  def initialize
-    @number = 10
+
+  def initialize(number)
+    @number = number
   end
 
   def number_of_bottles
     if @number >= 3
-    regular_verse
-  else
-    singular_verse
-  end
+      regular_verse
+    elsif @number == 2
+      singular_verse
+    else
+      final_verse
+    end
   end
 
   def regular_verse
@@ -20,15 +23,15 @@ There'll be #{@number - 1} green bottles hanging on the wall."
   end
 
   def singular_verse
-    "2 green bottles hanging on the wall
-2 green bottles hanging on the wall
+    "#{@number} green bottles hanging on the wall
+#{@number} green bottles hanging on the wall
 And if 1 green bottle should accidentally fall,
-There'll be 1 green bottle hanging on the wall."
+There'll be #{@number - 1} green bottle hanging on the wall."
   end
 
   def final_verse
-    "1 green bottle hanging on the wall
-1 green bottle hanging on the wall
+    "#{@number} green bottle hanging on the wall
+#{@number} green bottle hanging on the wall
 And if 1 green bottle should accidentally fall,
 There'll be no more bottles hanging on the wall."
   end
